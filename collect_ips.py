3cbd16e1e456654b4d0a9456621b109cdcf2cb69
Yishan556 <yunfei24@protonmail.com>
2025-10-01 13:22:31 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -17,7 +17,6 @@ logging.basicConfig(
 )
 
 URLS = [
-    'https://github.com/JiangXi9527/CNJX/blob/main/nihao.txt',
     'https://cf.090227.xyz',
     'https://ip.164746.xyz',
     'https://www.wetest.vip/page/cloudflare/address_v4.html',
