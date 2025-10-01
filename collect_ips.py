438ef33177702e2b3c0a964a7fd4c457541d719e
Yishan556 <yunfei24@protonmail.com>
2025-10-01 13:14:02 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -17,13 +17,13 @@ logging.basicConfig(
 )
 
 URLS = [
-    'https://bestip.badking.pp.ua',
     'https://github.com/JiangXi9527/CNJX/blob/main/nihao.txt',
     'https://cf.090227.xyz',
     'https://ip.164746.xyz',
     'https://www.wetest.vip/page/cloudflare/address_v4.html',
     'https://ipdb.api.030101.xyz/?type=bestcf&country=true',
     'https://ipdb.api.030101.xyz/?type=cfv4;cfv6&country=true',
+    'https://bestip.badking.pp.ua',
     'https://ipdb.api.030101.xyz/?type=bestproxy&country=true'
 ]
 
